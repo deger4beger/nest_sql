5a3cd29137102b4716b1390ca61671ec99a47915
deger4beger <deger.begerrr@gmail.com>
2022-01-25 20:37:40 +0600

    added more user tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,13 +1,20 @@
 import { Test } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-
+import { ValidationPipe } from '../shared/validation.pipe';
 
 describe('UserController', () => {
 
     let controller: UserController;
     const mockService = {
-
+        register: jest.fn((dto) => {
+            return {
+                id: Date.now(),
+                ...dto
+            }
+        }),
+        login: jest.fn((dto) => dto),
+        showAll: jest.fn((page) => page)
     }
 
     beforeEach(async () => {
@@ -22,6 +29,45 @@ describe('UserController', () => {
 
     it("controller should be defined", () => {
         expect(controller).toBeDefined()
+    })
+
+    it("should register new user", () => {
+        const dto = {
+            username: "name",
+            password: "morpex"
+        }
+        expect(controller.register(dto)).toEqual({
+            ...dto,
+            id: expect.any(Number)
+        })
+
+        expect(mockService.register).toHaveBeenCalledWith(dto)
+        expect(mockService.register).toHaveBeenCalledTimes(1)
+
+    })
+
+    it("should login user", () => {
+        const dto = {
+            username: "name",
+            password: "morpex"
+        }
+
+        expect(controller.login(dto)).toEqual(dto)
+
+        expect(mockService.login).toHaveBeenCalledWith(dto)
+        expect(mockService.login).toHaveBeenCalledTimes(1)
+
+    })
+
+    it("should get all users", () => {
+
+        const page = 3
+
+        expect(controller.showAllUsers(page)).toEqual(page)
+
+        expect(mockService.showAll).toHaveBeenCalledWith(page)
+        expect(mockService.showAll).toHaveBeenCalledTimes(1)
+
     })
 
 })
